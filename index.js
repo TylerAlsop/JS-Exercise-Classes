089b229200cbfe921b6293c7e7b2d97766ba8482
Tyler Alsop <tyleralsop@tylers-air.ad.uvu.edu>
2019-11-07 11:53:57 -0700

    Task 6: Added constructor and super to ProjectManager. Something weird is hapening in my npm watch

--- index.js
+++ index.js
@@ -204,8 +204,12 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor ({name, age, location, specialty, favLanguage, catchPhrase}){
+    super ({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  }
 }
 
 /*
